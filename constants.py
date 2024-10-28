808ff32a4776b07e2c22b8c9b8ce221f589718f2
Xinjing Zhou <zxjcarrot@gmail.com>
2024-10-28 18:44:33 +0000

    Update constants

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@
 
 # In the default IRQ affinity config mode
 # ID of the RX queue for each CPU 0-23
-CPU_TO_RX_QUEUE_MAP = [0, 6, 7, 8, 1, 9, 10, 11, 2, 12, 13, 14, 3, 15, 16, 17, 4, 18, 19, 20, 5, 21, 22, 23]
+CPU_TO_RX_QUEUE_MAP = [0, 6, 7, 8, 1, 9, 10, 11, 2, 12, 13, 14, 3, 15, 4, 5]
 
 # DDIO IO WAYS LLC mm register location
 DDIO_REG = 0xc8b
@@ -19,10 +19,10 @@ MAX_RULE_LOC = 1023
 
 # Maximum number of CPUs/connections
 # NOTE: This should be the same as number of CPUs
-MAX_CPUS = 24
+MAX_CPUS = 16
 CPUS = list(range(MAX_CPUS))
 MAX_CONNECTIONS = MAX_CPUS
-MAX_RPCS = 24
+MAX_RPCS = 16
 
 # Path to executables of profiling tools
 PERF_PATH = "/usr/bin/perf"
